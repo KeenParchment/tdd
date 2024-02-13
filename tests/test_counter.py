495578f8f1739ff942ed77367f0d513114a7fb82
Edward <edward1021kim@gmail.com>
2024-02-12 19:51:13 -0800

    CI lab continuation of TDD. Added .github/workflows/workflow.yml
    Adding the task 1 of the CI lab.

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,70 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+import json
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app
+
+# we need to import the file that contains the status codes
+from src import status
+
+
+class CounterTest(TestCase):
+    """Counter tests"""
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        result = self.client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        self._create_counter_and_assert('bar_duplicate', status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar_duplicate')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_read_a_counter(self):
+        """It should read a counter"""
+        counter_name = 'bar_read'
+        self._create_counter_and_assert(counter_name, status.HTTP_201_CREATED)
+
+        result = self.client.get(f'/counters/{counter_name}')
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        value = json.loads(result.data)[counter_name]
+        self.assertEqual(value, 0)
+
+        no_result = self.client.get('/counters/no_result_read')
+        self.assertEqual(no_result.status_code, status.HTTP_404_NOT_FOUND)
+        response_data = json.loads(no_result.data)
+        self.assertEqual(response_data["error"], "Counter not found")
+
+    def test_update_a_counter(self):
+        """It should update a counter"""
+        counter_name = 'bar_update'
+        self._create_counter_and_assert(counter_name, status.HTTP_201_CREATED)
+
+        base_result = self.client.get(f'/counters/{counter_name}')
+        base_value = json.loads(base_result.data)[counter_name]
+
+        update_result = self.client.put(f'/counters/{counter_name}')
+        self.assertEqual(update_result.status_code, status.HTTP_200_OK)
+
+        new_result = self.client.get(f'/counters/{counter_name}')
+        new_value = json.loads(new_result.data)[counter_name]
+        self.assertEqual(new_value, base_value + 1)
+
+        no_result = self.client.put('/counters/no_result_update')
+        self.assertEqual(no_result.status_code, status.HTTP_404_NOT_FOUND)
+        response_data = json.loads(no_result.data)
+        self.assertEqual(response_data["error"], "Counter not found")
+
+
+    # helper function
+    def _create_counter_and_assert(self, name, expected_status):
+        result = self.client.post(f'/counters/{name}')
+        self.assertEqual(result.status_code, expected_status)
